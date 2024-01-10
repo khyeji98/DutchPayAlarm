126587af6d5a4e88d97e061010d40340b0b548b5
khyeji98 <gpwl98wkd@gmail.com>
2024-01-10 23:41:59 +0900

    first commit

---
+++ DutchPayAlarm/DutchPayAlarm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DutchPayAlarm
+//
+//  Created by 김혜지 on 1/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DutchPayAlarm/DutchPayAlarm/DutchPayAlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  DutchPayAlarmApp.swift
+//  DutchPayAlarm
+//
+//  Created by 김혜지 on 1/10/24.
+//
+
+import SwiftUI
+
+@main
+struct DutchPayAlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
